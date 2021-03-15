5fb64d174c42c330944e4c14520b9f9866485a20
Ayça UÇAN <ayca.ucan@unluco.com>
2021-03-15 09:07:53 +0300

    Add project files.

---
+++ Quartz/Models/HotelDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Models
+{
+    public class CreateHotelDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength: 150, ErrorMessage = "hotel name is too long")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(maximumLength: 250, ErrorMessage = "address is too long")]
+        public string Address { get; set; }
+        [Required]
+        [Range(1,5)]
+        public double Rating { get; set; }
+        public int CountryId { get; set; }
+    }
+    public class HotelDTO : CreateHotelDTO
+    {
+        public int Id { get; set; }
+        public CountryDTO Country { get; set; }
+    }
+}
